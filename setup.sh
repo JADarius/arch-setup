52a25f00450791a265b301e687fa3a34cfd7dfb0
Darius-Andrei Jipa <jipad16@yahoo.com>
2023-08-10 15:33:06 +0300

    Added some new apps

--- setup.sh
+++ setup.sh
@@ -4,14 +4,15 @@
 sudo pacman -Syu --noconfirm
 # install the sound server
 sudo pacman --needed --noconfirm -S pipewire lib32-pipewire pipewire-docs \
-	wireplumber pipewire-audio
+	wireplumber pipewire-audio pipewire-pulse pulsemixer
 # install the good drivers
 # amd
 sudo pacman --needed --noconfirm -S xf86-video-amdgpu mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
 # install other packages
 sudo pacman --needed --noconfirm -S base-devel xorg-server xorg-xinit zip unzip mpc mpd neovim git xorg-apps \ 
 	firefox discord steam yt-dlp mpv ffmpeg acpilight dmenu ttf-hack-nerd ttf-meslo-nerd noto-fonts \
-	noto-fonts-cjk noto-fonts-emoji picom slock xclip ncmpcpp dunst maim lf ranger clang
+	noto-fonts-cjk noto-fonts-emoji picom slock xclip ncmpcpp dunst maim lf ranger clang sxiv \
+	zathura zathura-pdf-mupdf p7zip hsetroot python-pywal
 # create basic home folders
 sudo pacman --needed --noconfirm -S xdg-user-dirs
 xdg-user-dirs-update
@@ -23,6 +24,7 @@ mv *.sh ~
 # init the wallpapers folder
 mv Wallpapers ~
 curl -o ~/Wallpapers/1.jpg 'https://pbs.twimg.com/media/F19PMQSaMAEVGjS?format=jpg&name=4096x4096'
+wal -i ~/Wallpapers/1.jpg
 # mpd setup
 mkdir ~/.config/mpd
 mv mpd.conf ~/.config/mpd
@@ -38,7 +40,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 # paru setup
 git clone https://aur.archlinux.org/paru.git ~/paru
-cd paru
+cd ~/paru
 makepkg -sirc --noconfirm
 # neovim setup
 git clone https://github.com/JADarius/kickstart.nvim.git ~/.config/nvim
